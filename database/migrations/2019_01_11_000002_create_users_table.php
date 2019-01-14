c655de3e09b6f8f4e4b4f4a2850cadec1c68d61d
alumnos <sofia_santos_apps1ma1718@cev.com>
2019-01-14 11:14:32 +0100

    my first commit

---
+++ database/migrations/2019_01_11_000002_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',50);
+            $table->string('email',100)->unique()->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password',50);
+            $table->unsignedInteger('rol_id');
+            $table->foreign('rol_id')->references('id')->on('roles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2019_01_11_000005_locations.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Locations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('description', 140);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->double('x_coordinate');
+            $table->double('y_coordinate');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
